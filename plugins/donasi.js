39762f7cfc39545fd508127b8a4751a4a232d541
RielTod01 <107606413+RielTod01@users.noreply.github.com>
2022-07-04 15:38:04 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,14 +1,9 @@
 let handler = async m => m.reply(`
-╭─〘 *Donasi* 〙
-╭╡📮: Donasi ngab jgan gunain doang.
-│┝‷✧ *Pulsa:* 083813417529 [Axis]
+`››╭─〘 *Donasi* 〙
+╭╡🏮» Donasi ngab jangan gunain doang
+│┝ ➪ » 083813417529 (Axis)
 │╰───···─────
-│⁺◛˖ Ingin Donasi? Chat nomor dibawah
-│wa.me/${owner[0]}
-│*Bukan Bot!!!*
-│*Owner ${conn.user.name}*    
-│
-│
+│⁺◛˖ 
 ╰──────────···───╮`
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
