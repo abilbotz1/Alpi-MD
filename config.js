eecc4b930b31aebd435636906e016c478825e9d1
RielTod01 <107606413+RielTod01@users.noreply.github.com>
2022-07-04 12:17:59 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6285713137903'],
-  ['6285850539404'],
-  ['6285713137903', 'Owner', true]
+  ['6282146218274'],
+  ['6282146218274'],
+  ['6282146218274', 'OWNER RIELBOTZ', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@Alpi_bot'
-  var sticker_author = 'Alpi'
+  var sticker_name = 'RIELBOTZ'
+  var sticker_author = '2022'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '©Alpi'
+global.wm = '© *Riel*'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
